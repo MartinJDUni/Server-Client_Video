cc88fbdc0f488e725b9e08b80247c20527ff54d5
9re9o <gregorymena16@gmail.com>
2023-05-30 21:25:11 -0600

    feature client connect to server

--- videoTarea/src/Cliente/client.java
+++ videoTarea/src/Cliente/client.java
@@ -1,20 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Cliente;
 
-/**
- *
- * @author Martín
- */
-public class client {
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
-    /**
-     * @param args the command line arguments
-     */
+public class client {
+    private static final String SERVER_IP = "127.0.0.1"; // IP del servidor
+    private static final int SERVER_PORT = 3000; // Puerto del servidor
+    private static final String LOCAL_DIRECTORY = "videos/"; // Directorio local para almacenar los videos
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola mundo");
+        try {
+            Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+            System.out.println("Conectado al servidor " + SERVER_IP + ":" + SERVER_PORT);
+
+            // Leer la lista de videos disponibles desde el servidor
+            InputStream inputStream = socket.getInputStream();
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = bufferedInputStream.read(buffer);
+
+            System.out.println("Videos disponibles:");
+
+            while (bytesRead != -1) {
+                String videoName = new String(buffer, 0, bytesRead);
+                System.out.println(videoName);
+
+                // Descargar el video desde el servidor
+                downloadVideo(socket, videoName);
+
+                // Leer el siguiente nombre de video
+                bytesRead = bufferedInputStream.read(buffer);
+            }
+
+            // Cerrar la conexión
+            socket.close();
+            System.out.println("Conexión cerrada");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void downloadVideo(Socket socket, String videoName) throws IOException {
+        InputStream inputStream = socket.getInputStream();
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+        FileOutputStream fileOutputStream = new FileOutputStream(LOCAL_DIRECTORY + videoName);
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+            fileOutputStream.write(buffer, 0, bytesRead);
+        }
+
+        fileOutputStream.close();
+        System.out.println("Video descargado: " + videoName);
     }
 }
+
